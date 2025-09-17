b86592b37abbd7f1b27bef7007c1fe126eb20d14
Habiba <zain.habiba0@gmail.com>
2025-09-17 20:35:41 +0300

    create landing page

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color kLightYellow = Color.fromRGBO(253, 199, 110, 1);
+const Color kGrey = Color.fromRGBO(195, 195, 194, 1);
+const Color kProductListGrey = Color.fromRGBO(245, 245, 242, 1);
+
--- lib/landing/landing_page.dart
+++ lib/landing/landing_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sunglasses/constants.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({super.key});
@@ -11,7 +12,76 @@ class LandingPage extends StatelessWidget {
           SizedBox.expand(
             child: Image.asset('assets/landing.jpg', fit: BoxFit.fill),
           ),
+          Positioned(
+            right: 0,
+            left: 0,
+            bottom: 1,
+            child: Container(
+              height: 340,
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Glass \nfor eye',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 75,
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CustomButton(title: 'Start Shopping'),
+                      SizedBox(width: 20),
+                      Container(
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(40),
+                          color: Colors.white,
+                        ),
+                        child: const Icon(
+                          Icons.arrow_forward_rounded,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, this.color, required this.title});
+  final Color? color;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 22, horizontal: 30),
+      decoration: BoxDecoration(
+        color: color ?? kLightYellow,
+        borderRadius: BorderRadius.circular(35),
+      ),
+      child: Center(
+        child: Text(
+          title,
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w500,
+            fontSize: 22,
+          ),
+        ),
       ),
     );
   }
